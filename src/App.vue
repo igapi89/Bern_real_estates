8ae63ac7a445216f90ad5b66f7d6bc28e20d6057
igorapostolov <igor.apostolov@tatatu.com>
2023-09-12 17:44:14 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <p>Header</p>
+    </div>
+  </header>
+
+  <main>
+    <p>placeholder for map</p>
+  </main>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
